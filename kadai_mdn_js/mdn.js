b996049927097b1ec077e960c31cb2a1ab3b2f87
yonemuramio <151986427+yonemuramio@users.noreply.github.com>
2023-12-11 19:07:24 -0500

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,4 @@
+const date1 = new Date('August 22, 2023');
+const date2 = new Date('september 22, 2023');
+const date3 = new Date('August 22, 2023');
+console.log(date1.getUTCFullYear() + '年' +date2.getUTCMonth() + '月' + date3.getUTCDate() + '日');
