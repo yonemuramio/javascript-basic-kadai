7bd30996a769cbb1322c1abbce46469499d1d752
yonemuramio <151986427+yonemuramio@users.noreply.github.com>
2023-12-14 10:27:02 -0500

    Add files via upload

---
+++ kasai_20/event.js
@@ -0,0 +1,15 @@
+
+const newBtn = document.getElementById('btn');
+
+newBtn.addEventListener ('click', ()=> {
+    const text = document.getElementById('text');
+    text.textContent  = "ボタンをクリックしました";
+
+    }
+
+)
+
+
+
+
+
